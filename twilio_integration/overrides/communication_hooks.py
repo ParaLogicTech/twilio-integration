7410302353404612e92c157be1d1be5b304c0aac
Saif Ur Rehman <saif@paralogic.io>
2025-05-20 00:01:26 +0500

    fix(WA Msg): "Read" Delivery status, "Failed" WAMsg and delivery status

--- twilio_integration/overrides/communication_hooks.py
+++ twilio_integration/overrides/communication_hooks.py
@@ -6,24 +6,24 @@ from frappe.core.doctype.communication.communication import Communication
 class CommunicationTwilio(Communication):
 	def set_delivery_status(self, commit=False):
 		"""Look into the status of WhatsApp Queue linked to this Communication and set the Delivery Status of this Communication"""
-		delivery_status = None
-
 		if self.communication_medium != "WhatsApp":
 			super().set_delivery_status()
 			return
 
-		status_counts = Counter(
-			frappe.db.sql_list('''select status from `tabWhatsApp Message` where communication=%s''', self.name))
-
 		if self.sent_or_received == "Received":
 			return
 
+		status_counts = Counter(frappe.db.sql_list('''select status from `tabWhatsApp Message` where communication=%s''', self.name))
+
+		delivery_status = None
 		if status_counts.get("Queued") or status_counts.get("Not Sent") or status_counts.get("Sending"):
 			delivery_status = "Sending"
-		elif status_counts.get("Undelivered") or status_counts.get("Error"):
+		elif status_counts.get("Undelivered") or status_counts.get("Error") or status_counts.get("Failed"):
 			delivery_status = "Error"
-		elif status_counts.get("Sent") or status_counts.get("Delivered") or status_counts.get("Read"):
+		elif status_counts.get("Sent") or status_counts.get("Delivered"):
 			delivery_status = "Sent"
+		elif status_counts.get("Read"):
+			delivery_status = "Read"
 
 		if delivery_status:
 			self.db_set("delivery_status", delivery_status)
