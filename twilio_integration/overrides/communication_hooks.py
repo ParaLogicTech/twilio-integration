0c4362d62a97a56396f051347ce6a43b3bb1cb7f
Saif Ur Rehman <saif@paralogic.io>
2025-05-23 13:13:42 +0500

    feat(Communication): set read_by_recipient

--- twilio_integration/overrides/communication_hooks.py
+++ twilio_integration/overrides/communication_hooks.py
@@ -16,6 +16,8 @@ class CommunicationTwilio(Communication):
 		status_counts = Counter(frappe.db.sql_list('''select status from `tabWhatsApp Message` where communication=%s''', self.name))
 
 		delivery_status = None
+		read_by_recipient = 0
+
 		if status_counts.get("Queued") or status_counts.get("Not Sent") or status_counts.get("Sending"):
 			delivery_status = "Sending"
 		elif status_counts.get("Undelivered") or status_counts.get("Error") or status_counts.get("Failed"):
@@ -24,9 +26,13 @@ class CommunicationTwilio(Communication):
 			delivery_status = "Sent"
 		elif status_counts.get("Read"):
 			delivery_status = "Read"
+			read_by_recipient = 1
 
 		if delivery_status:
-			self.db_set("delivery_status", delivery_status)
+			self.db_set({
+				"delivery_status": delivery_status,
+				"read_by_recipient": read_by_recipient,
+			})
 			self.notify_change("update")
 			self.notify_update()
 
