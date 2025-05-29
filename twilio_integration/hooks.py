a7d6a652b20742223ae9dbd8b6ab42009e901500
PavanKumarPacharla <pavan@samasra.net>
2025-05-29 16:28:46 +0530

    [feature] :(#9) PR changes

--- twilio_integration/hooks.py
+++ twilio_integration/hooks.py
@@ -66,11 +66,9 @@ scheduler_events = {
 	],
 	"hourly_long": [
 		"twilio_integration.twilio_integration.doctype.whatsapp_message.whatsapp_message.flush_incoming_media_queue",
+		"twilio_integration.twilio_integration.doctype.whatsapp_message.whatsapp_message.reconcile_all_message_delivery_status",
 	],
 	"daily": [
 		"twilio_integration.twilio_integration.doctype.whatsapp_message.whatsapp_message.expire_whatsapp_message_queue",
-	],
-	"daily_long": [
-		"twilio_integration.twilio_integration.doctype.whatsapp_message.whatsapp_message.reconcile_all_message_delivery_status",
 	],
 }
--- twilio_integration/twilio_integration/twilio_handler.py
+++ twilio_integration/twilio_integration/twilio_handler.py
@@ -147,13 +147,13 @@ class Twilio:
 		return response
 
 	@classmethod
-	def get_message_status(cls, message_id):
+	def get_message(cls, message_id):
 		"""
 		Fetch the current status for this message from Twilio API and returns it
 		"""
 		client = cls.get_twilio_client()
 		message = client.messages(message_id).fetch()
-		return message.status.title()
+		return message
 
 class IncomingCall:
 	def __init__(self, from_number, to_number, meta=None):
